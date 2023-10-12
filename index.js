b753ce84b46cae6e4fcd5ca1f345bf28a10100ba
Paz <50664967+TwoWaySix@users.noreply.github.com>
2023-10-12 22:22:49 +0200

    added minimal code

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-alert("hi")
+async function fetchLibraryData(name) {
+    const response = await fetch(`https://registry.npmjs.org/${name}/latest`);
+    const data = await response.json();
+    return data;
+}
+
+async function showDependencies() {
+    const libraryName = document.getElementById("library-name").value;
+    const data = await fetchLibraryData(libraryName);
+    renderLibrary(data);
+}
+
+function renderLibrary(library) {
+    console.log(library);
+    let dependencies = "";
+    for (const [lib, version] of Object.entries(library.dependencies)) {
+        dependencies += `<li>${lib}: ${version}</li>`;
+    }
+
+    const element = document.getElementById("result");
+    element.innerHTML = `
+    <h1>${library.name}<sub>${library.version}</sub></h1>
+    <p>${library.description}</p>
+    <ul>${dependencies}</ul>`;
+}
